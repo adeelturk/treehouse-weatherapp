0f96cffe5331eba3a04601e89380ab98e558692f
Ian Sobo <dev.ian.sobo@gmail.com>
2015-04-29 16:59:12 -0400

    Am now in 'concurrency' section, error handling video

---
+++ app/src/main/java/com/example/ian/treehouseweatherapp/AlertDialogFragment.java
@@ -0,0 +1,19 @@
+package com.example.ian.treehouseweatherapp;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
+
+/**
+ * Created by ian on 4/29/2015.
+ */
+public class AlertDialogFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+    }
+}
--- app/src/main/java/com/example/ian/treehouseweatherapp/MainActivity.java
+++ app/src/main/java/com/example/ian/treehouseweatherapp/MainActivity.java
@@ -2,16 +2,23 @@ package com.example.ian.treehouseweatherapp;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
-import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
+    public static final MediaType mediaType = MediaType.parse("text/x-markdown; charset=utf-8");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +30,14 @@ public class MainActivity extends ActionBarActivity {
         double longitude = -122.423;
         String forecastURL = "https://api.forecast.io/forecast/" + apiKey +
                 "/" + latitude + "," + longitude;
-        String requestBinURL = "http://requestb.in/1cr03dh1";
 
-        // begin using OkHttp library recipe for Synchronous Get
-       /* OkHttpClient client = new OkHttpClient();
+         //begin using OkHttp library recipe for Synchronous Get
+        OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
-               /* .url(forecastURL)   .url(requestBinURL)
-                .build(); */
+               .url(forecastURL)   //.url(requestBinURL)
+                .build();
 
-        /*Call call = client.newCall(request);
+        Call call = client.newCall(request);
         call.enqueue(new Callback() {
             @Override
             public void onFailure(Request request, IOException e) {
@@ -40,21 +46,34 @@ public class MainActivity extends ActionBarActivity {
 
             @Override
             public void onResponse(Response response) throws IOException {
-
-                try {
+                try { Log.v(TAG, response.body().string());
                     if (response.isSuccessful()) {
-                        Log.v(TAG, response.body().string());
+
+                    } else {
+                        alertUserAboutError();
                     }
                 } catch (IOException e) {
                     Log.e(TAG, "Exception caught: ", e);
                 }
-
             }
-        });*/
-        OkHttpClient client = new OkHttpClient();
-        String dataToPost = "This is the string that will be posted!";
-        MediaType mediaType = MediaType.parse(dataToPost);
-        RequestBody requestBody = RequestBody.create(mediaType, dataToPost);
-        Request request = new Request.Builder().url(requestBinURL).post(requestBody).build();
+        });
+        Log.d(TAG, "Main UI code is running");
+    }
+
+    private void alertUserAboutError() {
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
